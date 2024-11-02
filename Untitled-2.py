c286a3a687b2cf7ff45ade5a45730dcf60eebf74
Nah'Teash Arianna Dorsey <nahteashdorsey@gmail.com>
2024-11-02 13:16:35 -0500

    Add files via upload

---
+++ Untitled-2.py
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+       <style>
+            body {
+                background-color: rgb(236, 230, 230);
+            } 
+            
+            body {
+        color: white;
+    }
+     h1 {
+            color: #FFDE21;
+         }
+         h2 {
+            text-align: center ;
+         }
+       </style>
+       <meta charset="utf-8">
+       <meta name="viewport" content="width=device-width, initial-scale=3.0">
+       <style>
+       * {
+         box-sizing: border-box;
+        }
+       
+       body {
+         margin: 0;
+       }
+       
+       /* Style the header */
+       .header {
+         background-color: #f1f1f1;
+         padding: 20px;
+         text-align: center;
+       }
+       
+       /* Style the top navigation bar */
+       .topnav {
+         overflow: hidden;
+         background-color: #333;
+       }
+       
+       /* Style the topnav links */
+       .topnav a {
+         float: left;
+         display: block;
+         color: #f2f2f2;
+         text-align: center;
+         padding: 14px 16px;
+         text-decoration: none;
+       }
+       
+       /* Change color on hover */
+       .topnav a:hover {
+         background-color: #ddd;
+         color: black;
+       }
+       
+       /* Create three equal columns that floats next to each other */
+       .column {
+         float: left;
+         width: 33.33%;
+         padding: 15px;
+       }
+       /* Clear floats after the columns */
+       .row::after {
+         content: "";
+         display: table;
+         clear: both;
+       }
+        /* Responsive layout - makes the three columns stack on top of each other instead of next to each other */
+       @media screen and (max-width:600px) {
+         .column {
+           width: 100%;
+         }
+       }
+       </style>
+             <head></head>
+       <body>
+       
+       <div class="header">
+         <h1>Movie Exclusives</h1>
+          <p> </p>
+
+       </div>
+       
+       <div class="topnav">
+       <a href="#" target="_blank">Shop Merch</a> 
+       </div>
+       
+       <body> 
+       <div>
+        <h1> Exclusive Movie Shop </h1>
+        <div>
+          <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for names..." title="Search for Shirts..."> 
+           let type = localStorage.getItem("giphy1.json")
+           console.log(storedValue)
+
+
+          
+          
+          </div>
+          <p></p>
+          <section>
+             <p></p>
+             <div id="output" class="output"></div>
+          </section>
+              </div>
+        <html>
+        <body>
+        <script>
+
+        
+        </script>
+        </body>
+        </html>
+         </body>  
+        </script>
+        </html>
+        <html>
+          <body>
+          </div>
+          </body>
+          </html>
+          
+          outputDiv.innerText 
+
+
+
+
+
+
+
+
+        
+        <head>
+        <link href='https://fonts.googleapis.com/css?family=Aguafina Script'
+        rel='stylesheet'>
+        <style>
+          body {font-family: 'Aguafina Script';font-size: 20px; color: rgba(184, 14, 14, 0.926); }
+        </style>
+
+        
+
+      
